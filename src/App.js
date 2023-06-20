9545f5613323533cda4deee32c9b7b5edc7f94dd
Damir <damir24may@mail.ru>
2023-06-20 23:50:43 +0300

    added redux persist

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     dispatch(addPositions(data));
-  }, []);
+  });
   return (
     <>
       <TheHeader />
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
 import { rootReducer } from "./root-reducer";
-export const store = createStore(
-  rootReducer,
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// настройки //(1)
+const persistConfig = {
+  key: "root",
+  storage,
+  // сохранять только ключи
+  whitelist: ["filters", "positions"],
+  // не сохранять  ключи
+  // blacklist: []
+};
+
+// обертка для работы с нашим rootReducer //(2)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//(3)
+const store = createStore(
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export { store };
+// передаем в корневой index.js //(4)
+export const persistor = persistStore(store);
