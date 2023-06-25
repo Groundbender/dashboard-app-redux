aeda7a8bf80dea0332da44957fefe17284161788
Damir <damir24may@mail.ru>
2023-06-25 17:51:01 +0300

    added redux-persist with кел

--- src/store.js
+++ src/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { combineReducers } from "@reduxjs/toolkit";
 import { filterReducer } from "./feature/filters/filter-slice";
 import { positionReducer } from "./feature/positions/positions-slice";
-export const store = configureStore({
-  reducer: {
-    filters: filterReducer,
-    positions: positionReducer,
-  },
-  devtools: true,
+// localStorage
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  PERSIST,
+  PAUSE,
+  PURGE,
+  REHYDRATE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  filters: filterReducer,
+  positions: positionReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devtools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, PERSIST, PAUSE, PURGE, REHYDRATE, REGISTER],
+      }, // игнорируем middleware из коробки rtk
+    }),
+});
+export const persistor = persistStore(store);
